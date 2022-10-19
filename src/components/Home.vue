07881cfde2dc47694b35170d387f02b3f3c4b96e
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2022-10-19 22:17:56 +0200

    landing page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div>
-        <h1>Welcome</h1>
-        <p>Hello</p>
+    <div class='p-12'>
+        <h1 class="font-bold font-headline text-8xl text-center text-gray-50">WELCOME</h1>
+        <p class="text-gray-50 text-center mt-4 text-2xl">Binerys Landing Page</p>
     </div>
 </template>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#8099e9',
+                secondary: '#e99dcb',
+            },
+            fontFamily: {
+                headline: ['Elianto'],
+            },
+        },
+    },
+    plugins: [],
 }
