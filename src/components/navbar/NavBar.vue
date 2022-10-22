a77ac8bd92443ac91c1bccc054ed669fc785a693
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2022-10-23 01:07:06 +0200

    navbar setup #3

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -1,21 +1,22 @@
 <template>
-    <header class="sticky top-0 max-h-11 bg-gray-700 py-1 shadow-lg">
-        <div class="mx-2 flex max-h-11 flex-row items-center gap-4 text-white">
+    <header class="sticky top-0 bg-gray-700 shadow-lg">
+        <div class="mx-2 flex flex-row items-center gap-x-4 text-white">
             <h1 class="font-headline text-4xl text-primary">BINERYS</h1>
-            <nav class="absolut top-0 flex flex-grow flex-row gap-4 pl-5 pt-10">
+            <nav class="absolut flex flex-grow translate-y-5 transform flex-row gap-x-4 pl-5">
                 <div
-                    class="rounded-full bg-gray-600 px-3 text-2xl"
+                    class="text-1xl rounded-full bg-gray-600 px-1"
                     v-for="navElement in navElements"
                     :key="navElement.name"
                 >
                     <router-link class="flex flex-row items-center" :to="{ name: navElement.link }">
-                        <i v-if="navElement.icon" :class="'m-2' + ' fa-solid fa-' + navElement.icon"></i>
+                        <i v-if="navElement.icon" :class="'m-1.5' + ' fa-solid fa-' + navElement.icon"></i>
                         <p class="mx-2 hidden md:flex">{{ navElement.name }}</p>
                     </router-link>
                 </div>
             </nav>
-            <label class="relative text-gray-500">
-                <i class="fa-solid fa-magnifying-glass absolute top-1/2 transform -translate-y-1/2 left-3"/>
+            <i class="fa-solid fa-magnifying-glass md:hidden text-white" />
+            <label class="relative hidden text-gray-500 md:flex">
+                <i class="fa-solid fa-magnifying-glass absolute top-1/2 left-3 -translate-y-1/2 transform" />
                 <input class="rounded-full px-2 pl-8" type="text" placeholder="search..." />
             </label>
 
